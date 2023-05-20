80f61ee0f841cd9e2b096d0b62c261b4a0dd3847
Mia <ewurama1894@gmail.com>
2023-05-20 14:29:26 +0000

    added operations

---
+++ js/myscript.js
@@ -0,0 +1,38 @@
+//arithmetic operators:
+
+//addition:
+console.log(150 + 27);
+//multiplication:
+console.log(3 * 2);
+//modulus:
+console.log(25 % 5);
+
+//comparison operators:
+//equal to:
+console.log(16 == 16);
+//greater than:
+console.log(34 > 0.5);
+//not identical to:
+console.log(89 != 789);
+
+//boolean operators:
+let num1 = 76
+let num2 = 40
+let num3 = 20
+let num4 = 10
+//and:
+console.log(num1 > num2 && num4 < num3);
+//or:
+console.log(num3 = num2 || num4 < num1);
+//not:
+console.log(!true);
+
+//string operators:
+//concatenation:
+let name = ("Ama");
+let name2 = ("Kwame");
+let number = 15;
+
+console.log("welcome, " + name);
+console.log("How are you " + name2 +" ?" );
+console.log("These are " + number + " mangoes");
