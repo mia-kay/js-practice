29fed3c9a86f9bf3dd0f8b59b9c0de15ad144662
mia-kay <130701960+mia-kay@users.noreply.github.com>
2023-07-07 09:31:26 +0000

    Update Bullet.js- comments

--- Bullet.js
+++ Bullet.js
@@ -1,4 +1,6 @@
 export default class Bullet {
+    //creates new instances of the bullet object with the specified
+    //initial properties. 
     constructor (canvas, x, y, velocity, bulletColor){
         this.canvas = canvas;
         this.x =x;
@@ -9,12 +11,17 @@ export default class Bullet {
         this.width = 5;
         this.height = 20;
     }
+
+    //updates the position of the bullet, sets the fill color, and draws
+    // the bullet 
     draw(ctx){
         this.y -= this.velocity;
         ctx.fillStyle = this.bulletColor;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 
+    //detects collision between the bullet and sprite object
+    //and returns true if there is a collision, or false if otherwise. 
     collideWith(sprite){
         if(this.x + this.width > sprite.x &&
              this.x < sprite.x + sprite.width &&
